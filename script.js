c24be02adeaa22d1ae7049a086d6bfcdb420a5eb
Auriale <laulaujitlala@gmail.com>
2025-07-21 17:08:28 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,2 @@
+alert("Attention !!⚠️ Ce site est suscpetible de vous brûler les yeux, non pas parce qu'il s'agit d'une boutique de bougies mais car il est actuellement en travaux 🚧");
+console.log("Bonjour")
